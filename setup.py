8a5d00500a44f168910907aa80c045317a8164e6
rstone <rstoneba@utdallas.edu>
2016-02-16 10:30:52 -0600

    gitignore

--- setup.py
+++ setup.py
@@ -3,21 +3,17 @@ import numpy.distutils.core
 import os
 
 #f2py -c --include-paths $CDF_INC -I$CDF_INC $CDF_LIB/libcdf.a -m fortran_cdf fortran_cdf.f -lm -lc
-
+base_cdf = '/Applications/cdf36_1-dist/'
 # setup fortran 90 extension
 #---------------------------------------------------------------------------  
 ext1 = numpy.distutils.core.Extension(
     name = 'fortran_cdf',
     sources = ['pysatCDF/fortran_cdf.f'], 
-    include_dirs = ['/Applications/cdf36_1-dist/include'],
-    f2py_options = ['--include-paths', '/Applications/cdf36_1-dist/include'],
+    include_dirs = [base_cdf + 'include'],
+    f2py_options = ['--include-paths', base_cdf +'include'],
     extra_link_args = ['-lm', '-lc'],
-    extra_objects = ['/Applications/cdf36_1-dist/lib/libcdf.a' ],
+    extra_objects = [base_cdf +'/lib/libcdf.a' ],
     extra_f77_compile_args = ['-lm', '-lc'],
-    #depends = ['/Applications/cdf36_1-dist/include', '/Applications/cdf36_1-dist/include/cdf.inc']
-    
-    
-    #depends = ['fortran_cdf.f'],
 
     )
 
