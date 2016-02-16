7e9c561e18f5e53e70a23df9f8e3e82132a82189
rstone <rstoneba@utdallas.edu>
2016-02-15 19:44:15 -0600

    setup.py now works, except for path detection of CDF_LIB

--- setup.py
+++ setup.py
@@ -2,24 +2,19 @@ import setuptools
 import numpy.distutils.core
 import os
 
+#f2py -c --include-paths $CDF_INC -I$CDF_INC $CDF_LIB/libcdf.a -m fortran_cdf fortran_cdf.f -lm -lc
+
 # setup fortran 90 extension
 #---------------------------------------------------------------------------  
 ext1 = numpy.distutils.core.Extension(
     name = 'fortran_cdf',
     sources = ['pysatCDF/fortran_cdf.f'], 
-               #'/Applications/cdf36_1-dist/include/cdf.inc'],
-    library_dirs = ['/Applications/cdf36_1-dist/lib','/Applications/cdf36_1-dist/include'],
-    libraries = ['libcdf.a', 'cdf.inc'],
-    #runtime_library_dirs = ['/Applications/cdf36_1-dist/lib','/Applications/cdf36_1-dist/include'],
-    include_dirs = ['/Applications/cdf36_1-dist/include','/Applications/cdf36_1-dist/lib'],
-    f2py_options = ['--include-paths', '/Applications/cdf36_1-dist/include',
-    '-I/Applications/cdf36_1-dist/include', '-lc', '-lm'], 
-                    #'-I', '/Applications/cdf36_1-dist/include',
-                    #'/Applications/cdf36_1-dist/lib/libcdf.a', '-l','m', '-l','c'],
+    include_dirs = ['/Applications/cdf36_1-dist/include'],
+    f2py_options = ['--include-paths', '/Applications/cdf36_1-dist/include'],
     extra_link_args = ['-lm', '-lc'],
-    extra_objects = ['/Applications/cdf36_1-dist/include/cdf.inc','/Applications/cdf36_1-dist/lib/libcdf.a' ],
-    extra_f77_compile_args = ['-I/Applications/cdf36_1-dist/include','-lm', '-lc'],
-    depends = ['/Applications/cdf36_1-dist/include', '/Applications/cdf36_1-dist/include/cdf.inc']
+    extra_objects = ['/Applications/cdf36_1-dist/lib/libcdf.a' ],
+    extra_f77_compile_args = ['-lm', '-lc'],
+    #depends = ['/Applications/cdf36_1-dist/include', '/Applications/cdf36_1-dist/include/cdf.inc']
     
     
     #depends = ['fortran_cdf.f'],
@@ -34,8 +29,8 @@ numpy.distutils.core.setup(
     name = 'pysatCDF',
     version = '0.1',        
     packages = ['pysatCDF'],
-    #package_data = {'': ['*.so', '*.so.*/*'],},
+    #package_data = {'': ['*.so', '*.so.*/*','*.so.*/*/*'],},
     #include_package_data = True  ,
-    #ext_modules = [ext1, ]    
+    ext_modules = [ext1, ]    
 
 )  
