bbc6da85107d24f33ebc5039d134845ed27354d6
Russell Stoneback <rstoneba@utdallas.edu>
2017-05-15 19:32:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ def CDF_build(self, ppath):
             'ENV=' + env_name,
             'CURSES=no',
             'SHARED=no',
-            'UCOPTIONS=-Dsingle_underscore -undefined',
+            'UCOPTIONS=-Dsingle_underscore -undefined -dynamic_lookup',
             'all',]
         cmd2 = ['make',
             'INSTALLDIR='+build_path,
