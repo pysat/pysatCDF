86256bfc0f0308273384b9b026073ea200695663
Russell Stoneback <rstoneba@utdallas.edu>
2022-02-24 15:41:02 -0600

    ENH: Added version per reviewer comment

--- pysatCDF/__init__.py
+++ pysatCDF/__init__.py
@@ -1,1 +1,14 @@
+"""pysatCDF is a simple reader for NASA's Common Data Format (CDF) files.
+
+pysatCDF uses NASA's C library to do the actual loading and couples
+Python to this library via an intermediate Fortran layer.
+
+"""
+# set version
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'version.txt')) as version_file:
+    __version__ = version_file.read().strip()
+
 from ._cdf import CDF as CDF
+
+del here
